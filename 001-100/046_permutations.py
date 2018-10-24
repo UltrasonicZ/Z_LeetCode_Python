06b429149a979e165381abdbda3f51b404f3c9db
UltrasonicZ <35156688+UltrasonicZ@users.noreply.github.com>
2018-10-24 22:56:44 +0800

    Update 046_permutations.py

--- 001-100/046_permutations.py
+++ 001-100/046_permutations.py
@@ -4,7 +4,14 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        res = []
+        if len(nums) <= 1:
+            return [nums]
+        for i in range(len(nums)):
+            nums[0], nums[i] = nums[i], nums[0]
+            for j in self.permute(nums[1:]):
+                res.append([nums[0]] + j)
+        return res
 
 
 if __name__ == "__main__":
